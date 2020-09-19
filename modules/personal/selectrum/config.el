6386b2bf422fe3b7374c98bb570968fe5f33ed68
lucas <lucas@chouette.vision>
2020-09-19 17:46:45 +0200

    Add module selectrum (testing the package)

---
+++ modules/personal/selectrum/config.el
@@ -0,0 +1,8 @@
+(use-package! selectrum
+  :config
+  (selectrum-mode -1))
+
+(use-package! selectrum-prescient
+  :after selectrum
+  :config
+  (selectrum-prescient-mode -1))
---
+++ modules/personal/selectrum/packages.el
@@ -0,0 +1,8 @@
+(package! selectrum
+  :recipe
+  (:host github :repo "raxod502/selectrum"))
+(package! selectrum-prescient
+  :recipe
+  (:host github
+   :repo "raxod502/prescient.el"
+   :files ("selectrum-prescient.el")))
